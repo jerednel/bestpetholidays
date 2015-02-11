71039f09bae09df28e10769b415e7a2c6a53f545
Jeremy Nelson <jerednel@gmail.com>
2015-02-11 20:06:13 +0000

    fixing error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
  
-  private
+  # private
  
-  def sign_up_params
-    params.require(:sitter).permit(:rate, :email, :website, :display_name, :post_code, :address_line_1, :address_line_2, :description, :password, :password_confirmation)
-  end
+  # def sign_up_params
+  #   params.require(:sitter).permit(:rate, :email, :website, :display_name, :post_code, :address_line_1, :address_line_2, :description, :password, :password_confirmation)
+  # end
  
-  def account_update_params
-    params.require(:sitter).permit(:rate, :email, :website, :display_name, :post_code, :address_line_1, :address_line_2, :description, :password, :password_confirmation, :current_password)
-  end
+  # def account_update_params
+  #   params.require(:sitter).permit(:rate, :email, :website, :display_name, :post_code, :address_line_1, :address_line_2, :description, :password, :password_confirmation, :current_password)
+  # end
 end
